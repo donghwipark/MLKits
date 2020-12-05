a02c0a7f640e33a27af2a7063e9ac8227300bd75
Donghwi Park <donghwipark@gmail.com>
2020-12-06 00:06:00 +0900

    knn algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -1,16 +1,27 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
-
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-  const bucket = _.chain(outputs)
-		.map((row) => [distance(row[0]), row[3]])
+	// Write code here to analyze stuff
+	const testSetSize = 100;
+	const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+	_.range(1, 20).forEach(k => {
+		const accuracy = _.chain(testSet)
+			.filter((testPoint) => knn(trainingSet, testPoint[0], k) === testPoint[3])
+			.size()
+			.divide(testSetSize)
+			.value();
+		console.log('for k : ' + k  + ' Accuracy:', accuracy);
+	})
+}
+
+function knn(data, point, k) {
+	return _.chain(data)
+		.map((row) => [distance(row[0], point), row[3]])
 		.sortBy((row) => row[0])
 		.slice(0, k)
 		.countBy((row) => row[1])
@@ -19,9 +30,16 @@ function runAnalysis() {
 		.last()
 		.first()
 		.parseInt()
-    .value();
-  console.log(`Your result will point in ${bucket}`);
+		.value();
 }
 
-const distance = (point) => Math.abs(point - predictionPoint);
+const distance = (pointA, pointB) => Math.abs(pointA - pointB);
 
+function splitDataset(data, testCount) {
+	const shuffled = _.shuffle(data);
+
+	const testSet = _.slice(shuffled, 0, testCount);
+	const trainingSet = _.slice(shuffled, testCount);
+
+	return [testSet, trainingSet];
+}
