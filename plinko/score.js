1ef93630551b12fc092d96f2008ac596607a8272
Park Donghwi <parkdonghwi@Parks-MBP-2.localdomain>
2020-12-11 19:42:33 +0900

    knn

--- plinko/score.js
+++ plinko/score.js
@@ -7,21 +7,31 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 function runAnalysis() {
 	// Write code here to analyze stuff
 	const testSetSize = 100;
-	const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+	const k = 10;
 
-	_.range(1, 20).forEach(k => {
+	
+	_.range(0, 3).forEach(feature => {
+		const data = _.map(row => [row[feature], _.last(row)]);
+		const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
 		const accuracy = _.chain(testSet)
-			.filter((testPoint) => knn(trainingSet, testPoint[0], k) === testPoint[3])
+			.filter(
+				(testPoint) => knn(trainingSet, _.initial(testPoint), k) === _.last(row)
+			)
 			.size()
 			.divide(testSetSize)
 			.value();
-		console.log('for k : ' + k  + ' Accuracy:', accuracy);
+		console.log('For feature of ' + feature  + ' Accuracy:', accuracy);
 	})
 }
 
 function knn(data, point, k) {
 	return _.chain(data)
-		.map((row) => [distance(row[0], point), row[3]])
+		.map((row) => {
+			return [
+				distance(_.initial(row), point),
+				_.last(row)
+			]
+		})
 		.sortBy((row) => row[0])
 		.slice(0, k)
 		.countBy((row) => row[1])
@@ -33,7 +43,13 @@ function knn(data, point, k) {
 		.value();
 }
 
-const distance = (pointA, pointB) => Math.abs(pointA - pointB);
+const distance = (pointA, pointB) => {
+	return _.chain(pointA)
+		.zip(pointB)
+		.map(([a, b]) => (a - b) ** 2)
+		.sum()
+		.value() ** 0.5
+};
 
 function splitDataset(data, testCount) {
 	const shuffled = _.shuffle(data);
@@ -42,4 +58,21 @@ function splitDataset(data, testCount) {
 	const trainingSet = _.slice(shuffled, testCount);
 
 	return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+	const clonedData = _.cloneDeep(data);
+
+	for (let i = 0; i < featureCount; i++) {
+		const column = clonedData.map(row => row[i]);
+
+		const min = _.min(column);
+		const max = _.max(column);
+
+		for (let j = 0; j < clonedData.length; j++) {
+			clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+		}
+	}
+
+	return clonedData;
 }
